5a0b9f0319e33516a7b9769f4fcb5f2989c98218
shashankx25 <shashankpatil250907@gmail.com>
2025-08-31 23:45:25 +0530

    COMPLETED

--- Day 1/hello.c
+++ Day 1/hello.c
@@ -3,3 +3,8 @@ int main() {
     printf("Hello, World!\n");
     return 0;
 }
+// This is a simple C program that prints "Hello, World!" to the console.
+// To compile and run this program, you can use the following commands in your terminal:
+// gcc hello.c -o hello
+// ./hello
+// Make sure you have GCC installed on your system to compile C programs.   
